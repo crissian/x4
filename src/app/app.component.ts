378725e439043c40f5e14930bdf8715384676742
Cristian Odea <crissian@gmail.com>
2018-12-12 14:30:46 +0200

    more functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Module } from '../services/module';
+import { Module, ModuleDefinition } from '../services/module';
 import { Resource } from '../services/resource';
 
 interface ModuleConfig {
@@ -7,20 +7,79 @@ interface ModuleConfig {
   count: number;
 }
 
-interface ResourceOutput {
-  resource: Resource;
-  amount: number;
+interface ModuleResourceOutput {
+  module: ModuleDefinition;
+  count: number;
+}
+
+interface ModuleResourceData {
+  moduleName: string;
+  count: number;
+  value: number;
+}
+
+class ResourceOutput {
+  private modules: ModuleResourceOutput[] = [];
+  expanded = false;
+
+  constructor(public resource: Resource) {
+  }
+
+  addModuleResource(module: ModuleDefinition, count: number) {
+    this.modules.push({ module: module, count: count });
+  }
+
+  get moduleResources() {
+    const result: ModuleResourceData[] = [];
+
+    this.modules.forEach(x => {
+      if (x.module.production != null && x.module.production.resource.id === this.resource.id) {
+        result.push({ count: x.count, moduleName: x.module.name, value: x.module.production.value });
+      }
+
+      const requirement = x.module.requirements.find(r => r.resource.id === this.resource.id);
+      if (requirement != null) {
+        result.push({ count: x.count, moduleName: x.module.name, value: -requirement.value });
+      }
+    });
+
+    return result;
+  }
+
+  get amount() {
+    let amount = 0;
+
+    this.modules.forEach(x => {
+      if (x.module.production != null && x.module.production.resource.id === this.resource.id) {
+        amount += x.count * x.module.production.value;
+      }
+
+      const requirement = x.module.requirements.find(r => r.resource.id === this.resource.id);
+      if (requirement != null) {
+        amount -= x.count * requirement.value;
+      }
+    });
+
+    return amount;
+  }
 }
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  title = 'X4';
   stationModules: ModuleConfig[] = [];
-  modules = Module.all;
+  modules = {
+    basicResources: Module.basicResources,
+    basicFood: Module.basicFood,
+    finalFood: Module.finalFood,
+    illegal: Module.illegal,
+    basicIntermediate: Module.basicIntermediate,
+    intermediateGoods: Module.intermediateGoods
+  };
+
+  expandState: { [resourceId: number]: boolean } = {};
 
   ngOnInit(): void {
     this.stationModules.push({ moduleId: 0, count: 0 });
@@ -37,28 +96,42 @@ export class AppComponent implements OnInit {
             let resource = resources.find(y => y.resource.id === r.resource.id);
 
             if (resource == null) {
-              resource = { amount: 0, resource: r.resource };
+              resource = new ResourceOutput(r.resource);
+              resource.expanded = this.expandState[r.resource.id] || false;
               resources.push(resource);
             }
 
-            resource.amount -= r.value * x.count;
+            resource.addModuleResource(module, x.count);
           });
 
           if (module.production != null) {
             let resource = resources.find(y => y.resource.id === module.production.resource.id);
 
             if (resource == null) {
-              resource = { amount: 0, resource: module.production.resource };
+              resource = new ResourceOutput(module.production.resource);
+              resource.expanded = this.expandState[module.production.resource.id] || false;
               resources.push(resource);
             }
 
-            resource.amount += module.production.value * x.count;
+            resource.addModuleResource(module, x.count);
           }
         }
       }
     });
 
     return resources;
+  }
+
+  removeModule(item: ModuleConfig) {
+    const index = this.stationModules.indexOf(item);
+    if (index >= 0) {
+      this.stationModules.splice(index, 1);
+    }
+  }
+
+  toggleExpanded(item: ResourceOutput) {
+    item.expanded = !item.expanded;
+    this.expandState[item.resource.id] = item.expanded;
   }
 
   addModule() {
