7fe0a30deef0bce52775730d9559a3b0b2d183cd
LokiSharp <loki.sharp@gmail.com>
2018-12-27 23:37:49 +0800

    Update Angular/Package config and Create nginx config template

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  googleAnalytics: 'UA-130883167-1'
+  googleAnalytics: 'UA-130883167-1',
+  languages: [
+    {code: 'en', label: 'English'},
+    {code: 'zh-Hans', label: '简体中文'},
+  ]
 };
