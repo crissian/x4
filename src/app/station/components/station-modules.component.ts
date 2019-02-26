330ff1e7c69fae709e766edbdedd9171a916d9d6
Cristian Odea <crissian@gmail.com>
2019-02-26 13:52:50 +0200

    v 2.0

--- src/app/station/components/station-modules.component.ts
+++ src/app/station/components/station-modules.component.ts
@@ -45,6 +45,7 @@ export class StationModulesComponent implements OnInit {
          .sort((a, b) => this.wareService.compareGroups(a.group, b.group));
 
       this.wareGroups.push({ name: ModuleTypes.habitation, modules: this.moduleService.getModulesByType(ModuleTypes.habitation) });
+      this.wareGroups.push({ name: ModuleTypes.buildmodule, modules: this.moduleService.getModulesByType(ModuleTypes.buildmodule) });
       this.wareGroups.push({ name: ModuleTypes.dockarea, modules: this.moduleService.getModulesByType(ModuleTypes.dockarea) });
       this.wareGroups.push({ name: ModuleTypes.pier, modules: this.moduleService.getModulesByType(ModuleTypes.pier) });
       this.wareGroups.push({ name: ModuleTypes.storage, modules: this.moduleService.getModulesByType(ModuleTypes.storage) });
