f73a7ec573df351b5934e2e64338e034bd6fd9c1
Cristian Odea <crissian@gmail.com>
2021-03-19 10:24:16 +0200

    fixed worker calculations

--- src/app/shared/services/data/workers-data.ts
+++ src/app/shared/services/data/workers-data.ts
@@ -1,46 +1,46 @@
-import {Races} from './race-data';
-import {Wares} from './wares-data';
+import { Races } from './race-data';
+import { Wares } from './wares-data';
 
 export const Workers = {
    argon: {
       race: Races.argon,
-      amount: 400,
+      amount: 200,
       consumption: [
-         {ware: Wares.foodrations, amount: 450},
-         {ware: Wares.medicalsupplies, amount: 270}
+         { ware: Wares.foodrations, amount: 450 },
+         { ware: Wares.medicalsupplies, amount: 270 }
       ]
    },
    teladi: {
       race: Races.teladi,
-      amount: 400,
+      amount: 200,
       consumption: [
-         {ware: Wares.nostropoil, amount: 228},
-         {ware: Wares.medicalsupplies, amount: 270}
+         { ware: Wares.nostropoil, amount: 228 },
+         { ware: Wares.medicalsupplies, amount: 270 }
       ]
    },
    paranid: {
       race: Races.paranid,
-      amount: 400,
+      amount: 200,
       consumption: [
-         {ware: Wares.sojahusk, amount: 286},
-         {ware: Wares.medicalsupplies, amount: 270}
+         { ware: Wares.sojahusk, amount: 286 },
+         { ware: Wares.medicalsupplies, amount: 270 }
       ]
    },
    split: {
       race: Races.split,
-      amount: 400,
+      amount: 200,
       consumption: [
-         {ware: Wares.cheltmeat, amount: 100},
-         {ware: Wares.scruffinfruits, amount: 140},
-         {ware: Wares.medicalsupplies, amount: 270}
+         { ware: Wares.cheltmeat, amount: 100 },
+         { ware: Wares.scruffinfruits, amount: 140 },
+         { ware: Wares.medicalsupplies, amount: 270 }
       ]
    },
    terran: {
-      race: Races.split,
-      amount: 400,
+      race: Races.terran,
+      amount: 200,
       consumption: [
-         {ware: Wares.terranmre, amount: 255},
-         {ware: Wares.medicalsupplies, amount: 270}
+         { ware: Wares.terranmre, amount: 255 },
+         { ware: Wares.medicalsupplies, amount: 270 }
       ]
    },
    get(method: string) {
