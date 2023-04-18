c2bbd4b1e3b607be257437042262a4c52213fd48
Cristian Odea <crissian@gmail.com>
2023-04-18 17:39:07 +0300

    more fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,9 +42,8 @@ export const routes: Routes = [
   }
 ];
 
-
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, { useHash: true }) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {
