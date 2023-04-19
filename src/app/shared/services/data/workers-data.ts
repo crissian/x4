a452e89f739626504976bc4ab409b57194449fd1
Cristian Odea <crissian@gmail.com>
2023-04-19 12:27:25 +0300

    fixed boron oasis requirements

--- src/app/shared/services/data/workers-data.ts
+++ src/app/shared/services/data/workers-data.ts
@@ -4,7 +4,7 @@ import { Wares } from './wares-data';
 export const Workers = {
    argon: {
       race: Races.argon,
-      amount: 200,
+      amount: 1200,
       consumption: [
          { ware: Wares.foodrations, amount: 450 },
          { ware: Wares.medicalsupplies, amount: 270 }
@@ -12,7 +12,7 @@ export const Workers = {
    },
    teladi: {
       race: Races.teladi,
-      amount: 200,
+      amount: 1200,
       consumption: [
          { ware: Wares.nostropoil, amount: 228 },
          { ware: Wares.medicalsupplies, amount: 270 }
@@ -20,29 +20,38 @@ export const Workers = {
    },
    paranid: {
       race: Races.paranid,
-      amount: 200,
+      amount: 1200,
       consumption: [
-         { ware: Wares.sojahusk, amount: 286 },
+         { ware: Wares.sojahusk, amount: 288 },
          { ware: Wares.medicalsupplies, amount: 270 }
       ]
    },
    split: {
       race: Races.split,
-      amount: 200,
+      amount: 1200,
       consumption: [
-         { ware: Wares.cheltmeat, amount: 100 },
-         { ware: Wares.scruffinfruits, amount: 140 },
+         { ware: Wares.cheltmeat, amount: 102 },
+         { ware: Wares.scruffinfruits, amount: 138 },
          { ware: Wares.medicalsupplies, amount: 270 }
       ]
    },
    terran: {
       race: Races.terran,
-      amount: 200,
+      amount: 1200,
       consumption: [
-         { ware: Wares.terranmre, amount: 255 },
+         { ware: Wares.terranmre, amount: 258 },
          { ware: Wares.medicalsupplies, amount: 270 }
       ]
    },
+  boron: {
+     race: Races.boron,
+    amount: 1200,
+    consumption: [
+      { ware: Wares.bofu, amount: 90 },
+      { ware: Wares.medicalsupplies, amount: 198 },
+      { ware: Wares.water, amount: 108 }
+    ]
+  },
    get(method: string) {
       return Workers[method] || Workers.argon;
    }
