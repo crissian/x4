01ddde1abc4df3434a66eacc6b6cd4813c992b8f
Cristian Odea <crissian@gmail.com>
2023-04-21 19:26:43 +0300

    fix import plans

--- src/app/station/components/import-plans.component.ts
+++ src/app/station/components/import-plans.component.ts
@@ -1,100 +1,109 @@
 import { Component, OnInit } from '@angular/core';
 import { ComponentBase } from '../../shared/components/component-base';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-// import * as convert from 'xml-js';
+import * as convert from 'xml-js';
 import { ModuleService } from '../../shared/services/module.service';
 import { LayoutService } from '../services/layout-service';
 import { Layout, ModuleConfig } from '../../shared/services/module-config';
 
 interface EntryAttribute {
-   macro: string;
+    macro: string;
 }
 
 interface Entry {
-   $: EntryAttribute;
+    _attributes: EntryAttribute;
 }
 
 interface PlanAttribute {
-   name: string;
+    name: string;
 }
 
 interface Plan {
-   $: PlanAttribute;
-   entry: Entry[];
+    _attributes: PlanAttribute;
+    entry: Entry[];
 }
 
 interface PlanList {
-   plan: Plan[];
+    plan: Plan[];
 }
 
 interface ConstructionPlan {
-   plans: PlanList;
+    plans: PlanList;
 }
 
 export interface ImportResult {
-   error: string;
-   layouts: string[];
+    error: string;
+    layouts: string[];
 }
 
 @Component({
-   selector: 'app-import-plans',
-   templateUrl: './import-plans.component.html'
+    selector: 'app-import-plans',
+    templateUrl: './import-plans.component.html'
 })
 export class ImportPlansComponent extends ComponentBase implements OnInit {
-   xml: string;
+    xml: string;
 
-   constructor(public activeModal: NgbActiveModal,
-               private moduleService: ModuleService,
-               private layoutService: LayoutService) {
-      super();
-   }
+    constructor(public activeModal: NgbActiveModal,
+                private moduleService: ModuleService,
+                private layoutService: LayoutService) {
+        super();
+    }
 
-   ngOnInit(): void {
-   }
+    ngOnInit(): void {
+    }
 
-   importPlans() {
-     // const converted = convert.xml2js(this.xml, { compact: true });
-     // console.log(converted);
-   }
+    importPlans() {
+        let layouts: string[];
+        let error: string = null;
 
-   private importCore(result: ConstructionPlan) {
-      const layouts: string[] = [];
+        try {
+            const result = convert.xml2js(this.xml, { compact: true }) as any;
+            layouts = this.importCore(result);
+        } catch (e) {
+            error = 'Failed to import layouts';
+        }
 
-      for (const plan of result.plans.plan) {
-         if (plan.entry == null || plan.entry.length === 0) {
-            continue;
-         }
+        this.activeModal.close({ error: error, layouts: layouts });
+    }
 
-         const name = plan.$.name;
+    private importCore(result: ConstructionPlan) {
+        const layouts: string[] = [];
 
-         const modules: ModuleConfig[] = [];
-
-         for (const item of plan.entry) {
-            const [ module ] = this.moduleService.getModulesByMacro(item.$.macro);
-            if (module) {
-               const existing = modules.find(x => x.moduleId === module.id);
-               if (!existing) {
-                  modules.push({ moduleId: module.id, count: 1 });
-               } else {
-                  existing.count++;
-               }
+        for (const plan of result.plans.plan) {
+            if (plan.entry == null || plan.entry.length === 0) {
+                continue;
             }
-         }
 
-         const layout: Layout = {
-            name: name,
-            config: modules
-         };
+            const name = plan._attributes?.name || 'Imported Plan';
 
-         this.layoutService.saveLayout(layout);
+            const modules: ModuleConfig[] = [];
 
-         layouts.push(name);
-      }
+            for (const item of plan.entry) {
+                const [ module ] = this.moduleService.getModulesByMacro(item._attributes.macro);
+                if (module) {
+                    const existing = modules.find(x => x.moduleId === module.id);
+                    if (!existing) {
+                        modules.push({ moduleId: module.id, count: 1 });
+                    } else {
+                        existing.count++;
+                    }
+                }
+            }
 
-      return layouts;
-   }
+            const layout: Layout = {
+                name: name,
+                config: modules
+            };
 
-   get canImport() {
-      return this.xml;
-   }
+            this.layoutService.saveLayout(layout);
+
+            layouts.push(name);
+        }
+
+        return layouts;
+    }
+
+    get canImport() {
+        return this.xml;
+    }
 }
